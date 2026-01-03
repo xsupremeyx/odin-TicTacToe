637b4f0f8c88800a8af8e5024423375c2ae90521
XSupremeYX <86472828+xsupremeyx@users.noreply.github.com>
2026-01-03 20:50:19 +0530

    Add Gameboard module to the js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("Hello World!");
+const Gameboard = (function() {
+    const board = [null,null,null,null,null,null,null,null,null];
+    const getBoard = () => board;
+    const setMark = (index,marker) => {
+        if(isEmpty(index)){
+            board.splice(index,1,marker);
+            return true;
+        }
+        return false;
+    };
+    const isEmpty = (index) => (board[index] === null);
+    const reset = () => board.fill(null);
+    return {getBoard, setMark, isEmpty, reset};
+})();
+
+
+
+const Player = (name, marker) => ({name,marker});
+const board = Gameboard;
+
+
+
